54465f6f0339eee7af3984d9c4a47c7d430c6c8b
pavan90-one <apavanreddy1003@gmail.com>
2022-04-06 15:28:08 +0530

    adding new code in app js

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const body_parser=require("body-parser");
 app.use(body_parser.json());
+app.get("/",(req,resp,next)=>{
+    resp.send("welcome  to node js deployment ")
+})
+
 
 const PORT = 5000;
 app.listen(PORT,()=>{
